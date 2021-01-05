fd8d0a1d79b31041754e9b425766fe1f829592f9
hunjin chung <hunjinchung0@gmail.com>
2021-01-05 10:55:13 +0000

    퀵정렬 추가

--- Sort/QuickSort.py
+++ Sort/QuickSort.py
@@ -26,3 +26,20 @@ def quick_sort(array, start, end):
 
 quick_sort(array, 0, len(array)-1)
 print(array)
+
+# 방법 2
+
+def quick_sort(array):
+
+  if len(array) <= 1:
+    return array
+
+  pivot = array[0]
+  tail = array[1:]
+
+  left_side = [x for x in tail if x <= pivot]
+  right_side = [x for x in tail if x > pivot]
+
+  return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
