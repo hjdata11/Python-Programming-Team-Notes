bffaa0747b179eaff5ecb4808a00ea2106de69b3
hunjin chung <hunjinchung0@gmail.com>
2021-01-11 09:59:36 +0000

    DP

---
+++ DP/AntWarrior.py
@@ -0,0 +1,16 @@
+
+# 개미 전사 (ai = i 번째 식량 창고 까지의 최적의 해))
+# 점화식 : ai = max(ai-1, ai-2 + k)
+
+n = int(input())
+
+array = list(map(int, input().split()))
+
+d = [0] * 100
+
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+for i in range(2, n):
+  d[i] = max(d[i-1], d[i-2]+array[i])
+
+print(d[n-1])
