0b524e179a5dc59c5ddf9a2af7faa03a945f4dfd
hunjin chung <>
2021-04-08 01:10:59 +0000

    [DP] 병사 배치하기

--- DP/SoldierPlacement.py
+++ DP/SoldierPlacement.py
@@ -1,17 +1,22 @@
 
 # 병사 배치하기 (가장 긴 증가하는 부분 수열(Longest Increasing Subsequence. LIS))
 # 점화식 : 모든 0 <= j < i 에 대하여, D[i] = max(D[i], D[j]+1) if array[j] < array[i]
+# https://www.acmicpc.net/problem/18353
+
+import sys
+sys.stdin = open("input.txt", 'r')
+input = sys.stdin.readline
 
 n = int(input())
-array = list(map(int, input().split()))
+List = list(map(int, input().split()))
+dp = [1 for i in range(n)]
 
-array.reverse()
+List = List[::-1]
 
-dp = [1] * n
+# 가장 긴 증가하는 부분 수열(LIS)
+for i in range(n):
+    for j in range(i):
+        if List[i] > List[j]:
+            dp[i] = max(dp[i], dp[j]+1)
 
-for i in range(1, n):
-  for j in range(0, i):
-    if array[j] < array[i]:
-      dp[i] = max(dp[i], dp[j]+1)
-
-print(n-max(dp))
+print(n - max(dp))
