c51871ee05bb19a91234883adaf607903983f444
hunjin chung <>
2021-04-17 12:26:03 +0000

    [BFS] 경쟁적 전염

--- BFSDFS/CompetitionContagious.py
+++ BFSDFS/CompetitionContagious.py
@@ -2,40 +2,43 @@
 # https://www.acmicpc.net/problem/18405
 
 import sys
-#sys.stdin = open("input.txt", "r")
+sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
 from collections import deque
 
 n, k = map(int, input().split())
-map = [ list(map(int, input().split())) for _ in range(n) ]
-a = input().split()
-target, x, y = int(a[0]), int(a[1]), int(a[2])
-location = []
+data = [ list(map(int, input().split())) for _ in range(n) ]
+tmp = input().split()
+s, x, y = int(tmp[0]), int(tmp[1]), int(tmp[2])
 
 dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+dy = [0, -1, 0, 1]
 
+viruses = []
+
+# 낮은 바이러스부터 증식
 def bfs():
-    global map
-    q = deque(location)
+
+    q = deque(viruses)
 
     while q:
-        virus, s, x, y = q.popleft()
-        if s == target:
+        value, time, a, b = q.popleft()
+        if time == s:
             break
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx >= 0 and ny >= 0 and nx < n and ny < n and map[nx][ny] == 0:
-                map[nx][ny] = virus
-                q.append([virus, s+1, nx, ny])
+            na = a + dx[i]
+            nb = b + dy[i]
+            if na >=0 and na < n and nb >= 0 and nb < n and data[na][nb] == 0:
+                data[na][nb] = value
+                q.append([value, time+1, na, nb])
 
+# 바이러스 위치
 for i in range(n):
     for j in range(n):
-        if map[i][j] > 0:
-            location.append([map[i][j], 0, i, j])
+        if data[i][j] > 0:
+            viruses.append([data[i][j], 0, i, j])
 
-location.sort()
+viruses.sort()
 
 bfs()
-print(map[x-1][y-1])
+print(data[x-1][y-1])
